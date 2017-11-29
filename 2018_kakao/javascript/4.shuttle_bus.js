d9c50fa95d11c6c71ea66f27618878c6b9096354
Donggoo Jung <dgjung0220@gmail.com>
2017-11-29 09:58:19 +0900

    shuttle bus

---
+++ 2018_kakao/javascript/4.shuttle_bus.js
@@ -0,0 +1,124 @@
+/*
+var timetable = ['08:00', '08:01', '08:02', '08:03'];
+const n = 1;
+const t = 1;
+const m = 5;
+*/
+/*
+var timetable = ['09:10', '09:09', '08:00'];
+const n = 2;
+const t = 10;
+const m = 2;
+*/
+/*
+var timetable = ['09:00', '09:00', '09:00', '09:00'];	
+const n = 2;
+const t = 1;
+const m = 2;
+*/
+/*
+var timetable = ['00:01', '00:01', '00:01', '00:01', '00:01'];
+const n = 1;
+const t = 1;
+const m = 5;
+*/
+/*
+var timetable = ['23:59'];
+const n = 1;
+const t = 1;
+const m = 1;
+*/
+
+var timetable = ['23:59','23:59', '23:59', '23:59', '23:59', '23:59', '23:59', '23:59', '23:59', '23:59', '23:59', '23:59', '23:59', '23:59', '23:59', '23:59'];
+const n = 10;
+const t = 60;
+const m = 45;
+
+var shuttle_time_table = new Map();
+
+var answer_format = (answer) => {
+    var answer = answer.split(':');
+    var hour = answer[0];
+    var minute = answer[1];
+
+    if (hour.length < 2) {
+        hour = '0' + hour;
+    }
+    if (minute.length < 2) {
+        minute = '0' + minute;
+    }
+    return hour+":"+minute;
+}
+
+var min_to_hour = (minute) => {
+    var hour = Math.floor(minute / 60);
+    var minute = minute - (hour * 60 );
+    
+    return hour+":"+minute;
+}
+
+var hour_to_min = (time) => {
+    var time = time.split(':');
+    var hour = parseInt(time[0]);
+    var minute = parseInt(time[1]);
+
+    return hour * 60 + minute;
+}
+
+var timetableSort = () => {
+    var new_timetable = new Array();
+    for (var i in timetable) {
+        new_timetable.push(hour_to_min(timetable[i]));
+    }
+    new_timetable = new_timetable.sort();
+    timetable = [];
+    for (var i in new_timetable) {
+        timetable.push(min_to_hour(new_timetable[i]));
+    }
+}
+
+var create_shuttle_time_table = () => {
+    shuttle_time_table.set("9:0",new Array());
+    for (var i = 1; i < n; i++) {
+        shuttle_time_table.set(min_to_hour(540+t*i),new Array());
+    }
+}
+
+var calculateCrewAboard = () => {
+    for (var [key, value] of shuttle_time_table) {
+        
+        var target_time = key;
+
+        for (var j = 0; j < timetable.length; j++) {
+            
+            if (timetable[j] !== undefined && shuttle_time_table.get(target_time).length < m && hour_to_min(timetable[j]) <= hour_to_min(target_time)) {
+                var array = shuttle_time_table.get(target_time);
+                array.push(timetable[j]);
+                shuttle_time_table.set(target_time, array);
+                timetable[j] = undefined;
+            }
+        }
+    }
+}
+
+var conAbleToAboard = () => {
+    var keys = Array.from(shuttle_time_table.keys());
+    var last_time = keys[keys.length-1];
+
+    if(shuttle_time_table.get(last_time).length < m) {
+        return last_time;
+    } else {
+        var array = shuttle_time_table.get(last_time);
+        return min_to_hour(hour_to_min(array[array.length-1]) - 1);
+    }
+}
+
+var solution = () => {
+    timetableSort();
+    create_shuttle_time_table();
+    calculateCrewAboard();
+    console.log(shuttle_time_table);
+    console.log(answer_format(conAbleToAboard()));
+}
+
+solution();
